66c904d88bc0679e53418c8a8351f5a3123d850f
PigCakee <cw145@mail.ru>
2020-12-09 10:12:00 +0300

    refactor: change ViewModelKey location

--- app/src/main/java/com/example/football/di/ViewModelFactory.kt
+++ app/src/main/java/com/example/football/di/ViewModelFactory.kt
@@ -20,12 +20,3 @@ class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Cl
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         viewModels[modelClass]?.get() as T
 }
-
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
