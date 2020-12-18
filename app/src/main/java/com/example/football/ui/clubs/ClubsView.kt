fe40a8fdf63079493c1f6379286eef67185c4aad
PigCakee <cw145@mail.ru>
2020-12-18 17:40:17 +0300

    feat: complete work with clubs MVP

--- app/src/main/java/com/example/football/ui/clubs/ClubsView.kt
+++ app/src/main/java/com/example/football/ui/clubs/ClubsView.kt
@@ -3,10 +3,13 @@ package com.example.football.ui.clubs
 import com.example.football.data.entity.Player
 import moxy.MvpView
 import moxy.viewstate.strategy.OneExecutionStateStrategy
+import moxy.viewstate.strategy.SingleStateStrategy
+import moxy.viewstate.strategy.SkipStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(value = OneExecutionStateStrategy::class)
 interface ClubsView: MvpView {
+    @StateStrategyType(value = SingleStateStrategy::class)
     fun setRecyclerData(list: MutableList<Pair<List<Player>, String>>)
+    @StateStrategyType(value = SkipStrategy::class)
     fun moveForward(club: String)
 }
