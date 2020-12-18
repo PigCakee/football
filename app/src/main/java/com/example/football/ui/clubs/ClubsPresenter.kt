da7dcad918f501c8f02f8fb9baffbed2929d2ffc
PigCakee <cw145@mail.ru>
2020-12-18 17:47:06 +0300

    feat: add disposables to clubs MVP

--- app/src/main/java/com/example/football/ui/clubs/ClubsPresenter.kt
+++ app/src/main/java/com/example/football/ui/clubs/ClubsPresenter.kt
@@ -3,6 +3,7 @@ package com.example.football.ui.clubs
 import com.example.football.data.entity.Player
 import com.example.football.data.repository.PlayersRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 import moxy.InjectViewState
@@ -14,17 +15,26 @@ class ClubsPresenter @Inject constructor(
     private val playersRepository: PlayersRepository
 ) : MvpPresenter<ClubsView>() {
 
+    private var clubsDisposable: Disposable? = null
+    private var playersDisposable: Disposable? = null
+
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         getClubs()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        clubsDisposable?.dispose()
+        playersDisposable?.dispose()
+    }
+
     private fun getClubs() {
         val list: MutableList<Pair<List<Player>, String>> = mutableListOf()
-        playersRepository.getAllClubsRX()
+        clubsDisposable = playersRepository.getAllClubsRX()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : DisposableObserver<List<String>>() {
+            .subscribeWith(object : DisposableObserver<List<String>>() {
                 override fun onNext(it: List<String>?) {
                     it?.forEach { club -> getPlayersByClub(club, list) }
                 }
@@ -35,10 +45,10 @@ class ClubsPresenter @Inject constructor(
     }
 
     private fun getPlayersByClub(club: String, list: MutableList<Pair<List<Player>, String>>) {
-        playersRepository.getPlayersByClubRX(club)
+        playersDisposable = playersRepository.getPlayersByClubRX(club)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : DisposableObserver<List<Player>>() {
+            .subscribeWith(object : DisposableObserver<List<Player>>() {
                 override fun onNext(it: List<Player>?) {
                     it?.let { list.add(Pair(it, club)) }
                     viewState.setRecyclerData(list)
